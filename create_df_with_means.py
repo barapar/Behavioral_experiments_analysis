86c1e1c268e2b6a5bfa17037975d35d634272396
Elizaveta Parfenova <parfenova.ev.2610@gmail.com>
2024-05-14 15:34:53 +0800

    means of behavioral answers loop

--- create_df_with_means.py
+++ create_df_with_means.py
@@ -4,14 +4,14 @@ from statistics import mean
 from os.path import join as opj
 
 
-experiment = opj('Sixth_exp', 'Sixth_cleaned_data')
+experiment = opj('Reproduction_exp', 'Reprod_cleaned_data')
 
 root_dir = opj('/', 'home', 'lisz', 'Desktop', 'Time_project', 'Time_project_exp')
 project_file = opj(root_dir, experiment, 'all_with_outliers.csv')
 
 df = pd.read_csv(project_file)
 
-df_new = pd.DataFrame(columns=['participant', 'pause_duration', 'mean'])
+df_new = pd.DataFrame(columns=['participant', 'pause_duration', 'pause_mean'])
 
 # create and sort pause list
 pause_list = sorted(df['pause_duration'].unique())
@@ -21,7 +21,7 @@ def create_row(sub, pause, df_new):
     mean_value = mean(subset['key_resp_rt'])
     new_row = {'participant': sub,
                'pause_duration': pause,
-               'mean': mean_value}
+               'pause_mean': mean_value}
     print(new_row)
     df_new = pd.concat([df_new, pd.DataFrame([new_row])], ignore_index=True)
     print(mean_value)
